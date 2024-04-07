eddecc5535e08e2feb76e4a02b424730c419b858
pradyumna0 <pradyumnadaware1@gmail.com>
2024-04-08 04:39:59 +0530

    trial

--- leapapps_assignment/settings.py
+++ leapapps_assignment/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-l#9d2t)&zf*+&w$w8#*^*iwsy=7&9t^su5uxyz7fk=5d8s9e(('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
