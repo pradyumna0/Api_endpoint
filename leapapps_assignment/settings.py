bdfa33640392d459c1be66d87bc2fdde23f4bf14
pradyumna0 <pradyumnadaware1@gmail.com>
2024-04-08 04:26:23 +0530

    trial

--- leapapps_assignment/settings.py
+++ leapapps_assignment/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS=os.path.join(BASE_DIR,'static')
 
 MEDIA_URLS ='/media/'
